65cf7de7dd902e9040351c1a59671f907c00b62b
Androx765 <Androx765@gmail.com>
2023-11-25 21:54:46 +0100

    erster richtiger push

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import epic_win from './assets/images/Win.png'
+import sad from './assets/images/sad.png'
+import mid from './assets/images/mid.png'
+import happy from './assets/images/happy.png'
 
-function App() {
-  const [count, setCount] = useState(0)
+const questions = [
+    {
+        question: 'Frage 1: Was ist die Hauptstadt von Deutschland?',
+        answers: ['Berlin', 'Paris', 'London', 'Rom'],
+        correctAnswer: 'Berlin'
+    },
+    {
+        question: 'Frage 2: Was ist die Hauptstadt von Frankreich?',
+        answers: ['Berlin', 'Paris', 'London', 'Rom'],
+        correctAnswer: 'Paris'
+    },
+    // Füge weitere Fragen hier hinzu
+];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+    const [win, setWin] = useState(false);
+    const [image, setImage] = useState(mid)
+    const correctPoints = 10;
+    const handleAnswerClick = (selectedAnswer) => {
+        let updatedScore = score; // Speichere den aktuellen Score
+        if (selectedAnswer === questions[currentQuestion].correctAnswer) {
+            updatedScore += correctPoints; // Aktualisiere den Score
+            setScore(updatedScore); // Setze den neuen Score
+            setImage(happy)
+        } else {
+            setImage(sad)
+        }
 
-export default App
+        // Hier könntest du den Code ergänzen, um zur nächsten Frage zu gehen
+
+        if (questions.length - 1 == currentQuestion) {
+            if (updatedScore < questions.length * correctPoints) {
+                setCurrentQuestion(0)
+                setScore(0)
+            } else {
+                setImage(epic_win)
+                setWin(true)
+            }
+        } else {
+            setCurrentQuestion(currentQuestion + 1);
+        }
+
+    };
+    const handleReplayClicked = () => {
+        setCurrentQuestion(0)
+        setScore(0)
+        setWin(false)
+        setImage(mid)
+    }
+
+    return (
+        <div className="App">
+            <div className="image-container">
+                <img src={image} alt="Bild" />
+            </div>
+            {win ?
+                <div>
+                    <h2>DU HAST GEWONNEN</h2>
+                    <button onClick={() => handleReplayClicked()}>
+                        Nochmal?
+                    </button>
+                </div>
+                :
+<div>
+    <div className="quiz-container">
+        <h2>{questions[currentQuestion].question}</h2>
+        <div className="answers">
+            {questions[currentQuestion].answers.map((answer, index) => (
+                <button key={index} onClick={() => handleAnswerClick(answer)}>
+                    {answer}
+                </button>
+            ))}
+        </div>
+    </div>
+</div>
+            }
+        </div >
+    );
+};
+
+export default App;
